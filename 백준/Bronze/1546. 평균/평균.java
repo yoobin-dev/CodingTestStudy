98c01b60c2260f175a55494bb795ada222949511
yoobin-dev <ybleedev@gmail.com>
2026-03-16 21:44:38 +0900

    [Bronze I] Title: 평균, Time: 212 ms, Memory: 18688 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,21 +1,22 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int A[] = new int[N];
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             A[i] = sc.nextInt();
         }
-        int M = 0;
         long sum = 0;
-        for(int i = 0; i < N; i++){
-            if(A[i] > M) M = A[i];
+        long max = 0;
+        for (int i = 0; i < N; i++) {
+            if(A[i] > max) {
+                max = A[i];
+            }
             sum += A[i];
         }
-        
-        System.out.print(sum * 100.0 / M / N);
+
+        System.out.println(sum * 100.0 / max / N);
     }
 }
