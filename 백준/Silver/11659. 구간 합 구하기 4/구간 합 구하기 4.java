cf9fc22a45ee66ad1298f51bdc88d727020dffdc
yoobin-dev <ybleedev@gmail.com>
2026-03-24 21:59:12 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1136 ms, Memory: 58724 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -2,22 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException  {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int totalCount = Integer.parseInt(st.nextToken());
-        int repeatCount = Integer.parseInt(st.nextToken());
-        long[] S = new long[totalCount + 1];
-        S[0] = 0;
-        st = new StringTokenizer(br.readLine());
-        for(int i = 1; i < totalCount + 1; i++){
-            S[i] = S[i - 1] + Integer.parseInt(st.nextToken());
-        }
-        for(int i = 0; i < repeatCount; i++){
+         StringTokenizer st = new StringTokenizer(br.readLine());
+         int numberCnt = Integer.parseInt(st.nextToken());
+         int questionCnt = Integer.parseInt(st.nextToken());
+
+         long[] S = new long[numberCnt + 1];
+         st = new StringTokenizer(br.readLine());
+         for(int i = 1; i <= numberCnt; i++) {
+            S[i] = S[i-1] + Integer.parseInt(st.nextToken());
+         }
+
+         for(int i = 0; i < questionCnt; i++) {
             st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            System.out.println(S[m] - S[n-1]);
-        }
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(S[b] - S[a-1]);
+         }
     }
 }
